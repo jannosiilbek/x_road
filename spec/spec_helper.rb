72c2437064cc62b8e31ac87fed5a2d7228ee80bf
Janno Siilbek <janno.siilbek@gmail.com>
2015-07-29 14:11:26 +0300

    change codeclimate order in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
-require 'bundler/setup'
-Bundler.setup
-
-require 'x_road'
-
 if RUBY_VERSION >= '1.9'
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
 end
+
+require 'bundler/setup'
+Bundler.setup
+require 'x_road'
 
 RSpec.configure do |config|
   # some (optional) config here
